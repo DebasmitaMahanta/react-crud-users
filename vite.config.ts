2a5e5a04a0ca64f7937f49c15a0026fedd9aca05
DebasmitaM98 <debasmitamahanta2803@gmail.com>
2026-02-07 12:21:09 +0530

    Setup JSON Server for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://react-crud-users-api.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), 
       },
